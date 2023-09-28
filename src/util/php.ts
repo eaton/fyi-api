cfb72e23629a0c3c5393b777e3ace0e8bd6d7f53
Jeff Eaton <jeff@eaton.fyi>
2023-09-28 16:39:46 -0500

    God, this is awful

--- src/util/php.ts
+++ src/util/php.ts
@@ -1,28 +1,22 @@
-  // discuss at: http://phpjs.org/functions/unserialize/
-  // original by: Arpad Ray (mailto:arpad@php.net)
-  // improved by: Pedro Tainha (http://www.pedrotainha.com)
-  // improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
-  // improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
-  // improved by: Chris
-  // improved by: James
-  // improved by: Le Torbi
-  // improved by: Eli Skeggs
-  // bugfixed by: dptr1988
-  // bugfixed by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
-  // bugfixed by: Brett Zamir (http://brett-zamir.me)
-  //  revised by: d3x
-  //    input by: Brett Zamir (http://brett-zamir.me)
-  //    input by: Martin (http://www.erlenwiese.de/)
-  //    input by: kilops
-  //    input by: Jaroslaw Czarniak
-  //        note: We feel the main purpose of this function should be to ease the transport of data between php & js
-  //        note: Aiming for PHP-compatibility, we have to translate objects to arrays
+import is from '@sindresorhus/is';
+
+// reworked from http://phpjs.org/ — gonna add some tests and refactor this eventually
+// but for now, well, it's an unholy abomination and everyone involved should feel bad
+// both that it exists and that it needs to exist.
+
+/**
+ * Given a string of serialized PHP data, attempt to parse it and return JSON-compatible
+ * primitives.
+ *
+ * @param {string} data
+ * @returns JSON-compatible primitives, hopefully.
+ */
+export function unserialize(data: string) {
   //   example 1: unserialize('a:3:{i:0;s:5:"Kevin";i:1;s:3:"van";i:2;s:9:"Zonneveld";}');
   //   returns 1: ['Kevin', 'van', 'Zonneveld']
   //   example 2: unserialize('a:3:{s:9:"firstName";s:5:"Kevin";s:7:"midName";s:3:"van";s:7:"surName";s:9:"Zonneveld";}');
   //   returns 2: {firstName: 'Kevin', midName: 'van', surName: 'Zonneveld'}
 
-function unserialize(data: string) {
   const utf8Overhead = function(chr: string) {
     // http://phpjs.org/functions/unserialize:571#comment_95906
     var code = chr.charCodeAt(0);
@@ -36,13 +30,13 @@ function unserialize(data: string) {
   };
 
   const read_until = function(data: string, offset: number, stopchr: string) {
-    let i = 2,
+    var i = 2,
       buf = [],
       chr = data.slice(offset, offset + 1);
 
     while (chr != stopchr) {
       if ((i + offset) > data.length) {
-        throw new Error('Invalid Data');
+        throw new Error('Invalid');
       }
       buf.push(chr);
       chr = data.slice(offset + (i - 1), offset + i);
@@ -50,7 +44,7 @@ function unserialize(data: string) {
     }
     return [buf.length, buf.join('')];
   };
-
+  
   const read_chrs = function(data: string, offset: number, length: number) {
     var i, chr, buf;
 
@@ -64,18 +58,34 @@ function unserialize(data: string) {
   };
 
   const _unserialize = function(data: string, offset: number) {
-    let dtype, dataoffset, keyandchrs, keys, contig,
-      length, array, readdata, readData, ccount,
-      stringlength, i, key, kprops, kchrs, vprops,
-      vchrs, value, chrs = 0;
+    let dtype: any;
+    let dataoffset: any;
+    let keyandchrs: any;
+    let keys: any;
+    let contig: any;
+    let length: any;
+    let array: any;
+    let readdata: any;
+    let readData: any;
+    let ccount: any;
+    let stringlength: any;
+    let i: any;
+    let key: any;
+    let kprops: any;
+    let kchrs: any;
+    let vprops: any;
+    let vchrs: any;
+    let value: any;
+    let chrs = 0;
 
-    let typeconvert = function(x: unknown) {
+    let typeconvert = function(x: any) {
       return x;
     };
 
     if (!offset) {
       offset = 0;
     }
+
     dtype = (data.slice(offset, offset + 1))
       .toLowerCase();
 
@@ -83,7 +93,7 @@ function unserialize(data: string) {
 
     switch (dtype) {
     case 'i':
-      typeconvert = function(x: string) {
+      typeconvert = function(x) {
         return parseInt(x, 10);
       };
       readData = read_until(data, dataoffset, ';');
@@ -123,7 +133,7 @@ function unserialize(data: string) {
       readdata = readData[1];
       dataoffset += chrs + 2;
       if (chrs != parseInt(stringlength, 10) && chrs != readdata.length) {
-        error('SyntaxError', 'String length mismatch');
+        throw new Error('SyntaxError: String length mismatch');
       }
       break;
     case 'a':
@@ -171,4 +181,98 @@ function unserialize(data: string) {
   };
 
   return _unserialize((data + ''), 0)[2];
+}
+
+/**
+ * Encode a JSON-compatible object in PHP's internal serialization format.
+ */
+export function serialize(mixed_value: any): string {
+  //   example 1: serialize(['Kevin', 'van', 'Zonneveld']);
+  //   returns 1: 'a:3:{i:0;s:5:"Kevin";i:1;s:3:"van";i:2;s:9:"Zonneveld";}'
+  //   example 2: serialize({firstName: 'Kevin', midName: 'van', surName: 'Zonneveld'});
+  //   returns 2: 'a:3:{s:9:"firstName";s:5:"Kevin";s:7:"midName";s:3:"van";s:7:"surName";s:9:"Zonneveld";}'
+
+  let val: any;
+  let key: any;
+  let okey: any;
+  let ktype: any = '';
+  let vals: any = '';
+  let count: any = 0;
+
+  const _utf8Size = function(str: string) {
+    var size = 0,
+      i = 0,
+      l = str.length,
+      code: string | number = '';
+    for (i = 0; i < l; i++) {
+      code = str.charCodeAt(i);
+      if (code < 0x0080) {
+        size += 1;
+      } else if (code < 0x0800) {
+        size += 2;
+      } else {
+        size += 3;
+      }
+    }
+    return size;
+  };
+
+  const _getType = function(inp: unknown) {
+    return is(inp).toString().toLocaleLowerCase();
+  };
+
+  const type = _getType(mixed_value);
+
+  switch (type) {
+  case 'function':
+    val = '';
+    break;
+  case 'boolean':
+    val = 'b:' + (mixed_value ? '1' : '0');
+    break;
+  case 'number':
+    val = (Math.round(mixed_value) == mixed_value ? 'i' : 'd') + ':' + mixed_value;
+    break;
+  case 'string':
+    val = 's:' + _utf8Size(mixed_value) + ':"' + mixed_value + '"';
+    break;
+  case 'array':
+  case 'object':
+    val = 'a';
+    /*
+        if (type === 'object') {
+          var objname = mixed_value.constructor.toString().match(/(\w+)\(\)/);
+          if (objname == undefined) {
+            return;
+          }
+          objname[1] = this.serialize(objname[1]);
+          val = 'O' + objname[1].substring(1, objname[1].length - 1);
+        }
+        */
+
+    for (key in mixed_value) {
+      if (mixed_value.hasOwnProperty(key)) {
+        ktype = _getType(mixed_value[key]);
+        if (ktype === 'function') {
+          continue;
+        }
+
+        okey = (key.match(/^[0-9]+$/) ? parseInt(key, 10) : key);
+        vals += serialize(okey) + serialize(mixed_value[key]);
+        count++;
+      }
+    }
+    val += ':' + count + ':{' + vals + '}';
+    break;
+  case 'undefined':
+    // Fall-through
+  default:
+    // if the JS object has a property which contains a null value, the string cannot be unserialized by PHP
+    val = 'N';
+    break;
+  }
+  if (type !== 'object' && type !== 'array') {
+    val += ';';
+  }
+  return val;
 }
