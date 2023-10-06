7c68ebb011dfafb190b1a5e06b06024dfb51a31f
Jeff Eaton <jeff@eaton.fyi>
2023-10-06 12:29:32 -0500

    Setting up a dummy cache for testing

--- src/serve.ts
+++ src/serve.ts
@@ -1,5 +1,6 @@
 import 'dotenv/config';
 import { getOAuth2Client } from "./imports/twitter/auth.js"
+import { Filestore } from './util/filestore.js';
 
 const auth = {
   apiKey: process.env.TWITTER_API_KEY,
@@ -11,6 +12,8 @@ const auth = {
   bearerToken: process.env.TWITTER_BEARER_TOKEN,
 };
 
+const fs = new Filestore({ cache: 'test/cache' });
+
 // const client = await getOAuth1Client(auth.apiKey!, auth.apiKeySecret!);
-const client = await getOAuth2Client(auth.clientId!, auth.clientSecret!);
+const client = await getOAuth2Client(auth.clientId!, auth.clientSecret!, fs);
 console.log(await client.currentUserV2());
