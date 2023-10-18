855e2eaec6638f9ab38615f1a1af5fed052470bb
Jeff Eaton <jeff@eaton.fyi>
2023-10-18 14:40:12 -0500

    Moving to Instagram on the little test runner

--- src/run.ts
+++ src/run.ts
@@ -1,17 +1,8 @@
 import 'dotenv/config';
 
-import { Drupal6Import } from './index.js';
+import { Instagram } from './index.js';
 
-const vpj = new Drupal6Import({
-  name: 'positiva-jeff-2008',
-  nodeTypesWithFields: ['oldsite', 'photo', 'review'],
-  ignoreNodeTypes: ['lj', 'amazonnode', 'banner'],
-  extraTables: ['delicious_link', 'delicious_tag', 'files', 'links', 'links_node', 'amazonitem', 'amazon_item', 'amazonnode'],
-  database: {
-    dbName: 'jeffblog-2008',
-    host: process.env.MYSQL_HOST,
-    user: process.env.MYSQL_USER,
-    pass: process.env.MYSQL_PASS,
-  }
-});
-await vpj.fillCache();
+const ig = new Instagram({
+  files: { input: process.env.INSTAGRAM_INPUT }
+})
+await ig.doImport();
