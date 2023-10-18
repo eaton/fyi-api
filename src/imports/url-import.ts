584c47a560f8383e4817c3d816e9fa9803d94094
Jeff Eaton <jeff@eaton.fyi>
2023-10-18 17:27:26 -0500

    Newly testable code for URL capture

--- src/imports/url-import.ts
+++ src/imports/url-import.ts
@@ -1,5 +1,7 @@
-import { Browser, BrowserContext, Page } from 'playwright';
+import is from '@sindresorhus/is';
+import { Browser, BrowserContext, Page, PageScreenshotOptions, chromium } from 'playwright';
 import { BaseImport, BaseImportOptions, extractWithCheerio, CheerioExtractTemplate} from "../index.js";
+import { fileTypeFromBuffer } from 'file-type';
 
 interface UrlImportOptions extends BaseImportOptions {
   /**
@@ -24,7 +26,7 @@ interface UrlImportOptions extends BaseImportOptions {
    * capture options will be used. If an array of `UrlScreenshotOptions` is passed
    * in, one image will be captured for each set of options.
    */
-  screenshot?: boolean | UrlScreenshotOptions | UrlScreenshotOptions[],
+  saveScreenshot?: boolean | UrlScreenshotOptions | UrlScreenshotOptions[],
 
   /**
    * Save the raw HTTP response data to a JSON file for later use.
@@ -41,25 +43,166 @@ interface UrlImportOptions extends BaseImportOptions {
    * If the URL resolves to a renderable page, save the page's rendered DOM
    * as an HTML file.
    */
-  savePage?: boolean,
+  saveDom?: boolean,
+
+  /**
+   * If the URL resolves to a renderable page, save it to a PDF file.
+   */
+  savePdf?: boolean | PdfOptions
 
   /**
    * If the page contains an HTML or XML payload, extract specific data from
    * it using the CheerioExtractor. See the `CheerioExtractTemplate` interface
    * for details.  
    */
-  savePageData?: CheerioExtractTemplate
+  saveData?: CheerioExtractTemplate
 }
 
-interface UrlScreenshotOptions {
+type PdfOptions = Parameters<Page['pdf']>[0];
+
+interface UrlScreenshotOptions extends PageScreenshotOptions {
   viewport?: {
     height: number,
     width: number,
-    scale?: number,
   },
-  format: 'jpeg' | 'png',
+}
+
+/**
+ * Option defaults
+ */
+
+const defaults: UrlImportOptions = {
+  saveDom: true
+}
+
+const screenshotDefaults: UrlScreenshotOptions = {
+  type: 'jpeg'
+}
+
+const pdfDefaults: PdfOptions = {
+  displayHeaderFooter: false,
 }
 
 export class UrlImport extends BaseImport {
+  declare options: UrlImportOptions;
+  protected browser?: Browser;
+  protected context?: BrowserContext;
+  protected page?: Page;
 
+  constructor(options?: UrlImportOptions) {
+    if (options?.saveScreenshot === true) {
+      options.saveScreenshot = screenshotDefaults;
+    }
+    if (options?.savePdf === true) {
+      options.savePdf = pdfDefaults;
+    }
+    super({ ...defaults, ...options });
+  }
+
+
+  /**
+   * Launches a browser and prepares it for use.
+   */
+  async setup(): Promise<Page> {
+    this.browser ??= await chromium.launch();
+    this.context ??= await this.browser.newContext();
+    this.page ??= await this.context.newPage();
+    return Promise.resolve(this.page);
+  }
+
+
+  /**
+   * Disposes of the pages and browser instance used for capture.
+   */
+  async teardown() {
+    return this.page?.close()
+      .then(() => this.context?.close())
+      .then(() => this.browser?.close())
+  }
+  
+  async fillCache() {
+    // Here's where we do the business
+  }
+
+  async captureUrl(url: string, name?: string, options?: UrlImportOptions) {
+    const opt = { ...this.options, ...options };
+    const page = await this.setup();
+    const response = await page.goto(url, { waitUntil: 'domcontentloaded' });
+
+    if (is.null(response)) {
+      this.log(`Couldn't load ${url}`);
+      return Promise.resolve();
+    }
+    
+    if (opt.saveBody || opt.saveRaw) {
+      const buffer = await response.body();
+
+      if (opt.saveBody) {
+        const extension = (await fileTypeFromBuffer(buffer))?.ext ?? 'bin';
+        await this.files.writeCache(`${name ?? 'page'}-body.${extension}`, buffer)
+      }
+  
+      if (opt.saveRaw) {
+        const raw = {
+          url: response.url(),
+          headers: await response.allHeaders(),
+          body: buffer.toString(),
+        }
+        await page.content().then(html => this.files.writeCache(`${name ?? 'page'}-response.json`, raw));
+      }
+    }
+    
+    if (opt.action) {
+      const result = await opt.action(page);
+      if (result === false) {
+        this.log(`Action callback aborted import of ${url}`);
+        return Promise.resolve();
+      }
+    }
+
+    if (opt.saveDom) {
+      await page.content().then(html => this.files.writeCache(`${name ?? 'page'}-dom.html`, html));
+    }
+
+    if (opt.savePdf && opt.savePdf !== true) {
+      const buffer = await page.pdf(opt.savePdf);
+      await this.files.writeCache(`${name ?? 'page'}.pdf`, buffer);
+    }
+
+    if (opt.saveScreenshot && opt.saveScreenshot !== true) {
+      const optset = Array.isArray(opt.saveScreenshot) ? opt.saveScreenshot : [opt.saveScreenshot];
+      for (const singleOpt of optset) {
+        await this.takeScreenshot(singleOpt, name);
+      }
+    }
+
+    if (opt.saveData) {
+      await page.content()
+        .then(html => extractWithCheerio(html, opt.saveData!))
+        .then(data => this.files.writeCache(`${name ?? 'page'}-data.json`, data));
+    }
+  }
+
+  async takeScreenshot(options: UrlScreenshotOptions, name?: string) {
+    const page = await this.setup();
+    if (options.viewport) {
+      await page.setViewportSize(options.viewport);
+    }
+    const buffer = await page.screenshot(options);
+    return this.files.writeCache(`${name ?? 'page'}.${options.type}`, buffer);
+  }
+}
+
+/**
+ * This helper function can be passed in as the action callback when capturing
+ * pages from Internet Archive Wayback Machine snapshots. It hides the Wayback
+ * navigator bar on screenshots and PDFs.
+ */
+export async function hideWayback(page: Page): Promise<void> {
+  if (!page.url().startsWith('https://web.archive.org/web/')) return Promise.resolve();
+  await page.evaluate(() => {
+    document.querySelector('#wm-ipp-base')?.remove();
+    document.querySelector('#wm-ipp-print')?.remove();
+  });
+  return Promise.resolve();
 }
