02d7d66178187a2b19b025edb2a13369c0a745f8
Jeff Eaton <jeff@eaton.fyi>
2023-11-01 18:01:43 -0500

    Trying out Zod

--- src/imports/autogram/autogram.ts
+++ src/imports/autogram/autogram.ts
@@ -1,10 +1,15 @@
-import slugify from "@sindresorhus/slugify";
 import { BaseImport, BaseImportOptions, uuid } from "../../index.js";
-import { sanitizeBio } from "./bio.js";
-import matter, { GrayMatterFile } from 'gray-matter';
+import matter from 'gray-matter';
+import { z } from 'zod';
+
+const partners: Record<string, string> = {
+  'jeff': 'e5228665-a06d-4d89-af72-b4fcb92f5d3b',
+  'karen': '5f839644-eb0a-475a-9274-d8b551837b26',
+  'ethan': 'b983462d-8c63-43ed-9ff3-c189ac6d4d21',
+};
 
 export interface AutogramImportOptions extends BaseImportOptions {
-
+  uuids?: Record<string, Record<string, string>>;
 }
 
 export interface AutogramImportCache extends Record<string, unknown> {
@@ -20,21 +25,18 @@ export class AutogramImport extends BaseImport<AutogramImportCache> {
 
   async fillCache(): Promise<void | AutogramImportCache> {
 
-    const bioFiles = await this.files.findInput('bios/*.md');
-    for (const file of bioFiles) {
-      const newEntity = await this.files.readInput(file)
-        .then(data => matter(data))
-        .then(matter => sanitizeBio(matter));
-      await this.files.writeCache(makeFileName(newEntity), newEntity);
-    }
-
     const linkFiles = await this.files.findInput('links/*.md');
     for (const file of linkFiles) {
-      const newEntity = await this.files.readInput(file)
-        .then(data => matter(data))
-        .then(matter => sanitizeBookmark(matter));
+      const raw = await this.files.readInput(file).then(data => matter(data));
+      const parsed = parseBookmark(raw);
+      await this.files.writeCache(`bookmarks/${parsed.date}-${uuid(parsed)}.json`, parsed)
+    }
 
-      await this.files.writeCache(makeFileName(newEntity), newEntity);
+    const clipFiles = await this.files.findInput('clips/*.md');
+    for (const file of clipFiles) {
+      const raw = await this.files.readInput(file).then(data => matter(data));
+      const parsed = parseAppearance(raw);
+      await this.files.writeCache(`appearances/${parsed.date}-${uuid(parsed)}.json`, parsed)
     }
 
     return Promise.resolve();
@@ -45,44 +47,83 @@ export class AutogramImport extends BaseImport<AutogramImportCache> {
   }
 }
 
-function makeFileName(entity: Record<string, unknown>) {
-  return entity?._type + '/' + uuid(entity) + '-' + slugify(entity.title as string) + '.json';
-}
+const GreyMatterSchema = z.object({
+  content: z.optional(z.string()),
+  excerpt: z.optional(z.string()),
+});
 
-export function sanitizeBookmark(matter: GrayMatterFile<Buffer>) {
+function parseBookmark(input: unknown) {
+  const BookmarkSchema = GreyMatterSchema.extend({
+    data: z.object({
+      title: z.string(),
+      slug: z.optional(z.string()),
+      date: z.optional(z.date()),
+      type: z.optional(z.string()),
+      link: z.optional(z.string().url()),
+    }),
+  });
+  const parsed = BookmarkSchema.parse(input);
+
   return {
     _type: 'bookmark',
-    title: matter.data.title,
-    teaser: matter.content.trim(),
-    date: matter.data.date,
-    link: matter.data.link
+    title: parsed.data.title,
+    teaser: ultraTrim(parsed.content),
+    date: parsed.data.date?.toISOString().split('T')[0],
+    type: parsed.data.type,
+    link: parsed.data.link,
   }
 }
 
-export function sanitizeAppearance(matter: GrayMatterFile<Buffer>) {
-  const partners: string[] = Array.isArray(matter.data?.partners) ? matter.data?.partners : [];
+function parseAppearance(input: unknown) {
+  const AppearanceSchema = GreyMatterSchema.extend({
+    data: z.object({
+      title: z.string(),
+      slug: z.optional(z.string()),
+      shortTitle: z.optional(z.string()),
+      hed: z.optional(z.string()),
+      dek: z.optional(z.string()),
+      description: z.optional(z.string()),
+      date: z.optional(z.date()),
+      link: z.optional(z.string().url()),
+      type: z.optional(z.string()),
+      partners: z.array(z.string()),
+      partnerRelationship: z.optional(z.string()),
+      venue: z.optional(z.object({
+        title: z.string(),
+        type: z.string(),
+        link: z.optional(z.string().url()),
+      }))
+    }),
+  });
+  const parsed = AppearanceSchema.parse(input);
+
   return {
     _type: 'appearance',
-    title: matter.data.title,
-    shortTitle: matter.data.shortTitle,
-    teaser: matter.data.dek ?? matter.data.description,
-    date: matter.data.date,
-    link: matter.data.link,
-    type: matter.data.type,
-    topics: [],
-    participants: partners.map(slug => {
+    title: parsed.data.title,
+    shortTitle: ultraTrim(parsed.data.shortTitle),
+    teaser: ultraTrim(parsed.data.dek ?? parsed.data.description),
+    date: parsed.data.date?.toISOString().split('T')[0],
+    link: parsed.data.link,
+    type: parsed.data.type,
+    participants: parsed.data.partners.map(slug => {
       return {
         _type: 'participant',
         person: {
-          _ref: `person-${slug}`,
+          _ref: partners[slug],
           _type: 'reference'
         },
-        role: matter.data.partnerRelationship
+        role: parsed.data.partnerRelationship
       }
     }),
-    venue: {
+    venue: parsed.data.venue ? {
       '_type': 'venue',
-      ...(matter.data.venue ?? {})
-    }
+      ...parsed.data.venue
+    } : undefined
   }
+}
+
+
+function ultraTrim(input: string | null | undefined) {
+  if (input) return input.trim().length ? input.trim() : undefined;
+  return undefined;
 }
