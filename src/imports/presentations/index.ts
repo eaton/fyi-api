40e64c65f426be64ead21f95c190fa283898f333
Jeff Eaton <jeff@eaton.fyi>
2023-11-18 17:13:17 -0600

    Just slamming presentations, man

--- src/imports/presentations/index.ts
+++ src/imports/presentations/index.ts
@@ -1,14 +1,25 @@
 import { Keynote, Disk, Text, KeynoteSlide, Image } from "mangler";
 
-const decks = Disk.find({ matching: '/Users/jeff/Library/Mobile Documents/com~apple~Keynote/Documents/**/*.key' });
+const source = Disk.dir('/Users/jeff/Library/Mobile Documents/com~apple~Keynote/Documents');
+let decks = source.find({ matching: '**/*.key' }).map(f => source.path(f));
+
 await migrateDecks(decks);
 
 export async function migrateDecks(files: string[]) {
-  const cache = Disk.dir('cache/presentations');
+  const cache = Disk.dir('output/presentations');
+
   for (const file of files) {
     let k = await Keynote.open(file);
+
     const slug = Text.toSlug(k.title);
-    const output = cache.dir(slug);
+    const output = (file.split('/').length > 1) ? cache.dir(file.split('/').slice(-2,-1).join('/')).dir(slug) : cache.dir(slug);
+
+    console.log(`Exporting ${k.title} to ${output.path()}`);
+
+    if (output.exists('index.md')) {
+      console.log(`Skipping ${output.path()}`);
+      continue;
+    }
 
     await k.export({ 
       format: 'JSON with images',
@@ -50,19 +61,21 @@ export async function migrateDecks(files: string[]) {
     const markdown = {
       data: {
         slug,
+        date: k.created,
         title: k.title,
         theme: k.theme,
         layout: 'presentation'
       },
       content: k.slides.filter(s => !s.skipped).map(s => makeMarkdownSlide(s)).join('\n\n---\n\n'),
     }
-    cache.dir(slug).write('index.md', markdown)
+    output.write('index.md', markdown);
+    await k.close();
   }
 }
 
 function makeMarkdownSlide(slide: KeynoteSlide): string {
   const output: string[] = [];
-  if (slide.title) output.push(`### ${slide.title}`);
+  if (slide.title) output.push(`### ${slide.title.replaceAll('\n', ' ')}`);
   if (slide.body) output.push(slide.body);
   output.push(`![Slide number ${slide.number}](images/images.${slide.number.toString().padStart(3, '0')}.jpeg)`);
   if (slide.notes) output.push(`${slide.notes}`);
